c58b4dd919205bb221e48d1a1daf1d3d574ee0c9
herczegAkos25 <akos.herczeg25@gmail.com>
2024-12-17 10:49:30 +0100

    Add files via upload

---
+++ FelHGY3.py
@@ -0,0 +1,38 @@
+import random
+
+szamok=[]
+nagyobb_0=0
+kisebb_0=0
+talalat=False
+
+for index in range(100):
+    szamok.append(random.randint(-100,100))
+
+for szam in szamok:
+    if szam > 0:
+        nagyobb_0 += 1
+    elif szam < 0:
+        kisebb_0 += 1
+
+for szam in szamok:    
+    if szam > 50:
+        print(f"50-nél nagyobb szám indexe: {szamok.index(szam)}")
+        talalat=True
+        break
+if talalat == False:
+    print(f"Nincs 50-nél nagyobb szám!")
+
+# segítséggel:
+for kulonbsegek in range(99): 
+    if (szamok[kulonbsegek] - szamok[kulonbsegek + 1] > 120) or (szamok[kulonbsegek + 1] - szamok[kulonbsegek] > 120):
+        print(f"Van olyan eset, hogy a két egymást követő szám meghaladja a 120-at")
+        break
+    
+if nagyobb_0 > kisebb_0:
+    print(f"Több pozitív szám van")
+else:
+    print(f"Több negatív szám van")
+    
+
+
+
