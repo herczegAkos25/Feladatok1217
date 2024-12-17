850e927ad259e8299de70436c17e588e7ee7a3ef
herczegAkos25 <akos.herczeg25@gmail.com>
2024-12-17 11:07:29 +0100

    Add files via upload

---
+++ DigKul5.py
@@ -0,0 +1,18 @@
+import random
+szamok=[]
+meredek_szakasz=0
+visszafele=0
+
+for i in range(30):
+    szamok.append(random.randint(0,9))
+
+for szam in szamok:
+    if szamok[szam] - szamok[szam + 1] >= 2:
+        meredek_szakasz += 1
+for szam in szamok:
+    szamok.reverse()
+    if szamok[szam] - szamok[szam + 1] >= 2:
+        visszafele += 1
+
+print(f"Meredek szakaszok száma az odafele vezető úton: {meredek_szakasz}")
+print(f"Meredek szakaszok száma a visszaúton: {visszafele} ")
